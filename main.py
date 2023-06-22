0a3d191f8c04e64cb32ac2ee9c90e5adf0504b8f
Yasin <f201213059@ktun.edu.tr>
2023-06-22 20:18:43 +0300

    fix: main file remark

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template, request
+from flask_login import LoginManager, login_user, UserMixin
+
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin):
+    def __init__(self, username):
+        self.id = username
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
 
 @app.route('/')
 def home():
@@ -10,14 +25,16 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        user_id = request.form['username']
         password = request.form['password']
 
-        if username == 'admin' and password == 'admin':
-            return render_template('dashborad.html',username=username)
+        if user_id == 'admin' and password == 'admin':
+            user = User(user_id)
+            login_user(user)
+            return render_template('dashboard.html',username=user_id)
         else:
             error = 'Invalid username or password'
-            return render_template('login.html', error=error)
+            return render_template('home.html', error=error)
 
     return render_template('login.html')
 
